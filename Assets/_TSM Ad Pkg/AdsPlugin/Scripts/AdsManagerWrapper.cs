fe43095d9fef8b69e024dbde17f41b9ac2b1412f
muzammaltsm-collab <muzammal.tsm@gmail.com>
2026-06-08 10:19:27 +0500

    Att panel Upgrade

--- Assets/_TSM Ad Pkg/AdsPlugin/Scripts/AdsManagerWrapper.cs
+++ Assets/_TSM Ad Pkg/AdsPlugin/Scripts/AdsManagerWrapper.cs
@@ -1,77 +1,146 @@
-using ByteBrewSDK;
 using GoogleMobileAds.Api;
 using GoogleMobileAds.Ump.Api;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
+#if UNITY_IOS
+using Unity.Advertisement.IosSupport;
+#endif
 
 public class AdsManagerWrapper : MonoBehaviour
 {
-
     public static AdsManagerWrapper Instance;
 
-    ConsentForm _consentForm;
+    [Header("ATT Panel")]
+    [SerializeField] private GameObject attPanel;
 
+    [Header("Ad Scripts")]
     public RewardedInterstitial rewardedInterstitial;
     public Interstitial _inter;
     public Rewarded _rewarded;
     public BannerAd _banner;
     public BannerAd _rectBanner;
     public AppOpen _appOpen;
-    //public  Interstitial _cp;
 
-    private static DateTime Time1ForAds;
+    private ConsentForm _consentForm;
 
-    public AdSize adaptiveSize = AdSize.GetLandscapeAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
-    #region init
+    public bool IsATTFlowFinished { get; private set; } = false;
+    public bool IsAdsSetupComplete { get; private set; } = false;
+
+    private bool adsInitialized = false;
+    private bool attChoiceDone = false;
+
+    public AdSize adaptiveSize =
+        AdSize.GetLandscapeAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
 
     private void Awake()
     {
-
         if (Instance != null && Instance != this)
         {
-            Destroy(this.gameObject);
+            Destroy(gameObject);
             return;
         }
 
         Instance = this;
-        //  DontDestroyOnLoad(this.gameObject);
+        // DontDestroyOnLoad(gameObject);
     }
 
-
-
-    #endregion
-
-    #region UserConsentForm
-    void Start()
+    private void Start()
     {
+        if (attPanel != null)
+            attPanel.SetActive(false);
+
+#if UNITY_IOS && !UNITY_EDITOR
+        if (ATTrackingStatusBinding.GetAuthorizationTrackingStatus() ==
+            ATTrackingStatusBinding.AuthorizationTrackingStatus.NOT_DETERMINED)
+        {
+            if (attPanel != null)
+            {
+                attPanel.SetActive(true);
+                return;
+            }
+        }
+#endif
+
+        IsATTFlowFinished = true;
+        RequestUMPConsent();
+    }
+
+    public void OnATTContinueButton()
+    {
+        if (attChoiceDone) return;
+
+        attChoiceDone = true;
+
+        if (attPanel != null)
+            attPanel.SetActive(false);
+
+        IsATTFlowFinished = true;
+
+#if UNITY_IOS && !UNITY_EDITOR
+        ATTrackingStatusBinding.RequestAuthorizationTracking();
+        StartCoroutine(ContinueAfterATT());
+#else
+        RequestUMPConsent();
+#endif
+    }
+
+    public void OnATTNotNowButton()
+    {
+        if (attChoiceDone) return;
+
+        attChoiceDone = true;
+
+        if (attPanel != null)
+            attPanel.SetActive(false);
+
+        IsATTFlowFinished = true;
+        RequestUMPConsent();
+    }
+
+    private IEnumerator ContinueAfterATT()
+    {
+        yield return new WaitForSeconds(1.5f);
+        RequestUMPConsent();
+    }
+
+    private void RequestUMPConsent()
+    {
+        ConsentRequestParameters request;
+
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
         var debugSettings = new ConsentDebugSettings
         {
             DebugGeography = DebugGeography.EEA,
             TestDeviceHashedIds = new List<string>
-        {
-            "3CB10DE262B4F50B8F94398D73192169"
-        }
+            {
+                "3CB10DE262B4F50B8F94398D73192169"
+            }
         };
 
-        ConsentRequestParameters request = new ConsentRequestParameters
+        request = new ConsentRequestParameters
         {
             TagForUnderAgeOfConsent = false,
-            ConsentDebugSettings = debugSettings,
+            ConsentDebugSettings = debugSettings
         };
+#else
+        request = new ConsentRequestParameters
+        {
+            TagForUnderAgeOfConsent = false
+        };
+#endif
 
         ConsentInformation.Update(request, OnConsentInfoUpdated);
     }
 
-    void OnConsentInfoUpdated(FormError error)
+    private void OnConsentInfoUpdated(FormError error)
     {
         if (error != null)
         {
-            Debug.LogError("ConsentInfoUpdate ErrorCode: " + error.ErrorCode);
-            Debug.LogError("ConsentInfoUpdate Message: " + error.Message);
+            Debug.LogError("Consent Update Error: " + error.Message);
+            InitializeAds();
             return;
         }
 
@@ -85,12 +154,11 @@ public class AdsManagerWrapper : MonoBehaviour
         }
     }
 
-    void OnLoadConsentForm(ConsentForm consentForm, FormError error)
+    private void OnLoadConsentForm(ConsentForm consentForm, FormError error)
     {
         if (error != null)
         {
-            Debug.LogError("LoadConsentForm ErrorCode: " + error.ErrorCode);
-            Debug.LogError("LoadConsentForm Message: " + error.Message);
+            Debug.LogError("Consent Form Load Error: " + error.Message);
             InitializeAds();
             return;
         }
@@ -107,167 +175,124 @@ public class AdsManagerWrapper : MonoBehaviour
         }
     }
 
-    void OnShowForm(FormError error)
+    private void OnShowForm(FormError error)
     {
         if (error != null)
-        {
-            Debug.LogError("ShowForm ErrorCode: " + error.ErrorCode);
-            Debug.LogError("ShowForm Message: " + error.Message);
-            return;
-        }
+            Debug.LogError("Consent Form Show Error: " + error.Message);
 
         InitializeAds();
     }
 
-    void InitializeAds()
+    private void InitializeAds()
     {
+        if (adsInitialized) return;
+
+        adsInitialized = true;
+        MobileAds.RaiseAdEventsOnUnityMainThread = true;
+
         MobileAds.Initialize(initStatus =>
         {
             Debug.Log("AdMob Initialized");
             LoadAllAds();
+            IsAdsSetupComplete = true;
         });
     }
-    #endregion
+
+    public void LoadAllAds()
+    {
+        if (_inter != null) _inter.LoadAd();
+        if (_rewarded != null) _rewarded.LoadAd();
+        if (_appOpen != null) _appOpen.LoadAd();
+
+        // if (rewardedInterstitial != null)
+        //     rewardedInterstitial.LoadAd();
+    }
 
     public void ShowInterstitial()
     {
-        if (PlayerPrefs.GetInt("RemoveAds") == 1)
-            return;
+        if (PlayerPrefs.GetInt("RemoveAds") == 1) return;
 
-        _inter.ShowAd();
+        if (_inter != null && _inter.IsAdAvailable)
+            _inter.ShowAd();
     }
-
-
 
     public bool IsInterstitialAvailable()
     {
-        if (PlayerPrefs.GetInt("RemoveAds") == 1)
-            return false;
-
-        return _inter.IsAdAvailable;
+        if (PlayerPrefs.GetInt("RemoveAds") == 1) return false;
+        return _inter != null && _inter.IsAdAvailable;
     }
 
-    public void ShowRewardedVideo(Action _Reward, Action _NoReward)
+    public void ShowRewardedVideo(Action reward, Action noReward)
     {
-        _rewarded.ShowAd(_Reward, _NoReward);
+        if (_rewarded != null && _rewarded.IsAdAvailable)
+            _rewarded.ShowAd(reward, noReward);
+        else
+            noReward?.Invoke();
     }
-
-
 
     public bool IsRewardedVideoAvailable()
     {
-        return _rewarded.IsAdAvailable;
+        return _rewarded != null && _rewarded.IsAdAvailable;
     }
+
     public void ShowBanner(AdPosition adPosition)
     {
+        if (PlayerPrefs.GetInt("RemoveAds") == 1) return;
 
-
-        if (PlayerPrefs.GetInt("RemoveAds") == 1)
-            return;
-
-
-        _banner.ShowAd(adPosition, AdsManagerWrapper.Instance.adaptiveSize);
+        if (_banner != null)
+            _banner.ShowAd(adPosition, adaptiveSize);
     }
+
     public void HideBanner()
     {
-
-        _banner.HideAd();
-
+        if (_banner != null)
+            _banner.HideAd();
     }
 
     public void ShowRectBanner(AdPosition adPosition)
     {
-        if (PlayerPrefs.GetInt("RemoveAds") == 1)
-            return;
+        if (PlayerPrefs.GetInt("RemoveAds") == 1) return;
 
-        _rectBanner.ShowAd(adPosition, AdSize.MediumRectangle);
-
+        if (_rectBanner != null)
+            _rectBanner.ShowAd(adPosition, AdSize.MediumRectangle);
     }
+
     public void HideRectBanner()
     {
-
-        _rectBanner.HideAd();
-
+        if (_rectBanner != null)
+            _rectBanner.HideAd();
     }
+
     public bool isBannerAvailable()
     {
-        return _banner.IsAdAvailable;
+        return _banner != null && _banner.IsAdAvailable;
     }
 
     public void ShowAppOpen()
     {
-        if (PlayerPrefs.GetInt("RemoveAds") == 1)
-            return;
+        if (PlayerPrefs.GetInt("RemoveAds") == 1) return;
 
-        _appOpen.ShowAd();
-        AdsHandler.Instance.AdShown = true;
+        if (_appOpen != null && _appOpen.IsAdAvailable)
+        {
+            _appOpen.ShowAd();
 
+            if (AdsHandler.Instance != null)
+                AdsHandler.Instance.AdShown = true;
+        }
     }
-
 
     public bool isAppOpenAvailable()
     {
-        return _appOpen.IsAdAvailable;
+        return _appOpen != null && _appOpen.IsAdAvailable;
     }
-
-
 
     public void showTestInter()
     {
         ShowInterstitial();
-
     }
 
     public void showTestRewarded()
     {
-        ShowRewardedVideo(null, null
-            );
+        ShowRewardedVideo(null, null);
     }
-    //public void showTestRewardedInter()
-    //{
-    //    ShowRewardedInter(null);
-    //}
-
-
-
-
-    public void LoadAllAds()
-    {
-
-
-        _inter.LoadAd();
-
-
-
-        _rewarded.LoadAd();
-
-
-
-
-        _appOpen.LoadAd();
-
-        //rewardedInterstitial.LoadAd();
-    }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
